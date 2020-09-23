5e020eeb56b835a8c0d35d1db0320c453f6b7f39
Anshuman Das <adas@ts.com>
2020-09-23 16:39:20 +0530

    add events table

---
+++ test/pns_web/controllers/event_controller_test.exs
@@ -0,0 +1,101 @@
+defmodule PnsWeb.EventControllerTest do
+  use PnsWeb.ConnCase
+
+  alias Pns.Account
+
+  @create_attrs %{
+    end_time: ~N[2010-04-17 14:00:00],
+    html_template: "some html_template",
+    key: "7488a646-e31f-11e4-aace-600308960662",
+    start_time: ~N[2010-04-17 14:00:00],
+    website_url: "some website_url"
+  }
+  @update_attrs %{
+    end_time: ~N[2011-05-18 15:01:01],
+    html_template: "some updated html_template",
+    key: "7488a646-e31f-11e4-aace-600308960668",
+    start_time: ~N[2011-05-18 15:01:01],
+    website_url: "some updated website_url"
+  }
+  @invalid_attrs %{end_time: nil, html_template: nil, key: nil, start_time: nil, website_url: nil}
+
+  def fixture(:event) do
+    {:ok, event} = Account.create_event(@create_attrs)
+    event
+  end
+
+  describe "index" do
+    test "lists all events", %{conn: conn} do
+      conn = get(conn, Routes.event_path(conn, :index))
+      assert html_response(conn, 200) =~ "Listing Events"
+    end
+  end
+
+  describe "new event" do
+    test "renders form", %{conn: conn} do
+      conn = get(conn, Routes.event_path(conn, :new))
+      assert html_response(conn, 200) =~ "New Event"
+    end
+  end
+
+  describe "create event" do
+    test "redirects to show when data is valid", %{conn: conn} do
+      conn = post(conn, Routes.event_path(conn, :create), event: @create_attrs)
+
+      assert %{id: id} = redirected_params(conn)
+      assert redirected_to(conn) == Routes.event_path(conn, :show, id)
+
+      conn = get(conn, Routes.event_path(conn, :show, id))
+      assert html_response(conn, 200) =~ "Show Event"
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, Routes.event_path(conn, :create), event: @invalid_attrs)
+      assert html_response(conn, 200) =~ "New Event"
+    end
+  end
+
+  describe "edit event" do
+    setup [:create_event]
+
+    test "renders form for editing chosen event", %{conn: conn, event: event} do
+      conn = get(conn, Routes.event_path(conn, :edit, event))
+      assert html_response(conn, 200) =~ "Edit Event"
+    end
+  end
+
+  describe "update event" do
+    setup [:create_event]
+
+    test "redirects when data is valid", %{conn: conn, event: event} do
+      conn = put(conn, Routes.event_path(conn, :update, event), event: @update_attrs)
+      assert redirected_to(conn) == Routes.event_path(conn, :show, event)
+
+      conn = get(conn, Routes.event_path(conn, :show, event))
+      assert html_response(conn, 200) =~ "some updated html_template"
+    end
+
+    test "renders errors when data is invalid", %{conn: conn, event: event} do
+      conn = put(conn, Routes.event_path(conn, :update, event), event: @invalid_attrs)
+      assert html_response(conn, 200) =~ "Edit Event"
+    end
+  end
+
+  describe "delete event" do
+    setup [:create_event]
+
+    test "deletes chosen event", %{conn: conn, event: event} do
+      conn = delete(conn, Routes.event_path(conn, :delete, event))
+      assert redirected_to(conn) == Routes.event_path(conn, :index)
+
+      assert_error_sent 404, fn ->
+        get(conn, Routes.event_path(conn, :show, event))
+      end
+    end
+  end
+
+  defp create_event(_) do
+    event = fixture(:event)
+    {:ok, event: event}
+  end
+end
