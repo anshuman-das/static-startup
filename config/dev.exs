96194dea5ff7cbf1e2bf56760df246a0ac80a37a
Deeinr <dsingh>
2021-01-22 11:53:17 +0530

    socket communications

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :pns, Pns.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :pns, PnsWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4002],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
