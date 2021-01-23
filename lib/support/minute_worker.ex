027710f1d13673666ca388f2a93dd2cb794df783
Deeinr <dsingh>
2021-01-23 10:24:57 +0530

    setting up channel based communications

--- lib/support/minute_worker.ex
+++ lib/support/minute_worker.ex
@@ -5,7 +5,23 @@ defmodule Support.Workers.MinuteWorker do
 
   @impl Oban.Worker
   def perform(_) do
-    events = EventService.get_all_active_events() |> IO.inspect(label: "--")
+    events =
+      EventService.get_all_active_events()
+      |> IO.inspect(label: "--")
+      |> IO.inspect(label: "events==> ")
+
+    events
+    |> Enum.map(fn event ->
+      event_details = %{application_id: event.application_id, id: event.id}
+
+      #  Pns.Endpoint.broadcast!("notifier:" <> rid, "new_msg", %{uid: "uid", body: "body"})
+      PnsWeb.Endpoint.broadcast!("notifier:lobby", "event_started", %{
+        uid: "uid",
+        body: event_details
+      })
+      |> IO.inspect(label: "Inspect : ")
+    end)
+
     {:ok, events}
   end
 end
